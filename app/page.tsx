b4962b6ea52c610f437f2c110b90374b8230c009
kinetic4 <pratyushbhargava6@gmail.com>
2026-01-25 17:59:00 +0530

    Refactor homepage layout to include CompanionCard, CompanionList, and CTA components, replacing previous content with a more structured design.

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,24 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionList from '@/components/CompanionList'
+import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline '>Welcome to my saas</h1>
-      <Button>
-        Lets get started
-      </Button>
-    </div>
+    <main>
+      <h1 className='text-2xl underline '>Popular Companion</h1>
+      <section className='home-section'>
+        <CompanionCard />
+        <CompanionCard />
+        <CompanionCard />
+      </section>
+      
+      <section className='home-secton'>
+        <CompanionList />
+        <CTA />
+      </section>
+    </main>
   )
 }
 
