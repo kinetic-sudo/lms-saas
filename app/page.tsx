a8c08a451d35b09f987328d579b91607c7ca51fa
kinetic4 <pratyushbhargava6@gmail.com>
2026-01-26 02:22:28 +0530

    Refactor CompanionList component to accept dynamic props for title and companions, and implement a table layout for displaying recent sessions with subject colors and navigation links.

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import CompanionCard from '@/components/CompanionCard'
 import CompanionList from '@/components/CompanionList'
 import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
+import { recentSessions } from '@/constants'
 import React from 'react'
 
 const Page = () => {
@@ -36,7 +37,11 @@ const Page = () => {
       </section>
       
       <section className='home-section'>
-        <CompanionList />
+        <CompanionList 
+          title = "Recently completed session"
+          companions = {recentSessions}
+          classNames = "w-2/3 max-lg:w-full" 
+        />
         <CTA />
       </section>
     </main>
--- components/CompanionList.tsx
+++ components/CompanionList.tsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table"
 
-const CompanionList = () => {
+  import { cn, getSubjectColor } from "@/lib/utils";
+import Link from "next/link";
+import Image from "next/image";
+
+  interface CompanionProps {
+    title: string;
+    companions?: Companion[]
+    classNames?: string 
+  }
+
+
+const CompanionList = ({title, companions, classNames} : CompanionProps ) => {
   return (
-    <div>CompanionList</div>
+    <article className={cn( "companion-list", classNames)}>
+        <h2 className="font-bold text-3xl">Recent Session</h2>
+        <Table >
+  <TableHeader>
+    <TableRow>
+      <TableHead className="text-lg w-2/3">Lessons</TableHead>
+      <TableHead className="text-lg">Subject</TableHead>
+      <TableHead className="text-lg text-right">Duration</TableHead>
+    </TableRow>
+  </TableHeader>
+  <TableBody>
+    {companions?.map(({id, subject, topic, duration, name}) => (
+        <TableRow key={id}>
+            <TableCell>
+                <Link href={`/companions/${id}`}>
+                   <div className="flex items-center gap-2">
+                      <div className="size-[72px] flex items-center justify-center rounded-lg max-md:hidden" style={{backgroundColor: getSubjectColor(subject)}}>
+                       <Image src={`/icons/${subject}.svg`} alt = {subject} width = {35} height = {35}/>
+                      </div>
+                   </div>
+                </Link>
+            </TableCell>
+        </TableRow>
+    ))}
+ 
+  </TableBody>
+</Table>
+    </article>
   )
 }
 
