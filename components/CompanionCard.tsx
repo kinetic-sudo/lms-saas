72d7c27b42b62f8df3fc3b7aadde60ecaba7aa3c
kinetic4 <pratyushbhargava6@gmail.com>
2026-01-26 01:54:07 +0530

    Add CompanionCard enhancements: include subject badge, duration display, and navigation link for launching lessons

--- components/CompanionCard.tsx
+++ components/CompanionCard.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 interface CompanionCardProps {
     id: string
     name: string
@@ -11,7 +13,25 @@ interface CompanionCardProps {
 const CompanionCard = ({id, name, topic, subject, duration, color} : CompanionCardProps) => {
   return (
     <article className="companion-card" style={{background: color}}>
-
+        <div className="flex justify-between items-center">
+            <div className='subject-badge'>
+                {subject}
+            </div>
+            <button className="companion-bookmark">
+                <img src="/icons/bookmark.svg" alt="bookmark" width={12.5} height={15}/>
+            </button>
+        </div>
+        <h2 className="text-2xl font-bold">{name}</h2>
+        <p className="text-sm">{topic}</p>
+        <div className="flex items-center gap-2">
+            <img src="/icons/clock.svg" alt="duration" width={13.5} height={13.5}/>
+            <p className="text-sm">{duration} Minutes</p>
+        </div>
+        <Link href={`/companion/${id}`} className="w-full" >
+          <button className="btn-primary w-full justify-center">
+            Launch Lesson 
+          </button>
+        </Link>
     </article>
   )
 }
