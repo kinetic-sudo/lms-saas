664109cef9efc2e78a381aeebc6d3aacdf7c0a45
kinetic4 <pratyushbhargava6@gmail.com>
2026-01-26 22:14:52 +0530

    Update package dependencies to include @radix-ui/react-select and @radix-ui/react-separator; implement CompanionForm with enhanced validation and UI components for improved user experience.

--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -2,8 +2,25 @@
 
 import * as z from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { Controller, useForm } from "react-hook-form"
+import { Controller,  useForm } from "react-hook-form"
 import {toast} from "sonner"
+import { Button } from "./ui/button"
+import {
+    Field,
+    FieldError,
+    FieldGroup,
+    FieldLabel,
+  } from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+  } from "@/components/ui/select"
+import { subjects } from "@/constants"
+
 
 const formSchema = z.object({
     name: z
@@ -43,25 +60,158 @@ const CompanionForm = () => {
         }
     })
 
-    function onSubmit(data: z.infer<typeof formSchema>) {
-        toast("You submitted the following values:", {
-          description: (
-            <pre className="bg-code text-code-foreground mt-2 w-[320px] overflow-x-auto rounded-md p-4">
-              <code>{JSON.stringify(data, null, 2)}</code>
-            </pre>
-          ),
-          position: "bottom-right",
-          classNames: {
-            content: "flex flex-col gap-2",
-          },
-          style: {
-            "--border-radius": "calc(var(--radius)  + 4px)",
-          } as React.CSSProperties,
-        })
+    const onSubmit = (values: z.infer<typeof formSchema>) => {
+       console.log(values)
       }
 
   return (
-    <div>CompanionForm</div>
+    
+    <form id="form-rhf-demo" onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+    <FieldGroup>
+      <Controller
+        name="name"
+        control={form.control}
+        render={({ field, fieldState }) => (
+          <Field data-invalid={fieldState.invalid}>
+            <FieldLabel htmlFor="form-rhf-demo-title">
+              Companion Name
+            </FieldLabel>
+            <Input
+              {...field}
+              className="input"
+              id="form-rhf-demo-title"
+              aria-invalid={fieldState.invalid}
+              placeholder="Enter the companion name"
+              autoComplete="off"
+            />
+            {fieldState.invalid && (
+              <FieldError errors={[fieldState.error]} />
+            )}
+          </Field>
+        )}
+      />
+       <Controller
+        name="subject"
+        control={form.control}
+        render={({ field, fieldState }) => (
+          <Field data-invalid={fieldState.invalid}>
+            <FieldLabel htmlFor="form-rhf-demo-title">
+              Subject Name
+            </FieldLabel>
+            <Select onValueChange={field.onChange} value={field.value} defaultValue={field.value}>
+  <SelectTrigger className="input capitalize">
+    <SelectValue placeholder="Select the subject" />
+  </SelectTrigger>
+  <SelectContent>
+    {subjects.map((subject) => (
+        <SelectItem 
+          value={subject}
+          key={subject}
+          className="capitalize"
+        >
+        {subjects}
+       </SelectItem>
+    ))}
+    </SelectContent>
+</Select>
+            {fieldState.invalid && (
+              <FieldError errors={[fieldState.error]} />
+            )}
+          </Field>
+        )}
+      />
+       <Controller
+        name="topic"
+        control={form.control}
+        render={({ field, fieldState }) => (
+          <Field data-invalid={fieldState.invalid}>
+            <FieldLabel htmlFor="form-rhf-demo-title">
+              Topic Name
+            </FieldLabel>
+            <Input
+              {...field}
+              className="input"
+              id="form-rhf-demo-title"
+              aria-invalid={fieldState.invalid}
+              placeholder="Enter the companion name"
+              autoComplete="off"
+            />
+            {fieldState.invalid && (
+              <FieldError errors={[fieldState.error]} />
+            )}
+          </Field>
+        )}
+      />
+       <Controller
+        name="style"
+        control={form.control}
+        render={({ field, fieldState }) => (
+          <Field data-invalid={fieldState.invalid}>
+            <FieldLabel htmlFor="form-rhf-demo-title">
+              Style Name
+            </FieldLabel>
+            <Input
+              {...field}
+              className="input"
+              id="form-rhf-demo-title"
+              aria-invalid={fieldState.invalid}
+              placeholder="Enter the style name"
+              autoComplete="off"
+            />
+            {fieldState.invalid && (
+              <FieldError errors={[fieldState.error]} />
+            )}
+          </Field>
+        )}
+      />
+       <Controller
+        name="voice"
+        control={form.control}
+        render={({ field, fieldState }) => (
+          <Field data-invalid={fieldState.invalid}>
+            <FieldLabel htmlFor="form-rhf-demo-title">
+              Voice Name
+            </FieldLabel>
+            <Input
+              {...field}
+              className="input"
+              id="form-rhf-demo-title"
+              aria-invalid={fieldState.invalid}
+              placeholder="Enter the voice name"
+              autoComplete="off"
+            />
+            {fieldState.invalid && (
+              <FieldError errors={[fieldState.error]} />
+            )}
+          </Field>
+        )}
+      />
+         <Controller
+        name="duration"
+        control={form.control}
+        render={({ field, fieldState }) => (
+          <Field data-invalid={fieldState.invalid}>
+            <FieldLabel htmlFor="form-rhf-demo-title">
+              Duration Name
+            </FieldLabel>
+            <Input
+              {...field}
+              className="input"
+              id="form-rhf-demo-title"
+              aria-invalid={fieldState.invalid}
+              placeholder="Enter the duration name"
+              autoComplete="off"
+            />
+            {fieldState.invalid && (
+              <FieldError errors={[fieldState.error]} />
+            )}
+          </Field>
+        )}
+      />
+      
+    </FieldGroup>
+    <Button type="submit">Submit</Button>
+  </form>
   )
 }
 
