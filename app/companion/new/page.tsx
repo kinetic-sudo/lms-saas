44c4d4ba5aff4ca02bfb751a389d92bca3db20c5
kinetic4 <pratyushbhargava6@gmail.com>
2026-01-26 05:38:32 +0530

    Refactor NewCompanion page layout to include CompanionForm, enhancing the user interface with a structured design and improved accessibility.

--- app/companion/new/page.tsx
+++ app/companion/new/page.tsx
@@ -1,7 +1,14 @@
+import CompanionForm from "@/components/CompanionForm"
 
 const NewCompanion = () => {
   return (
-    <div>New companion</div>
+    <main className="lg:w-1/2 md:w-1/3 items-center justify-center">
+        <article className="w-full flex flex-col gap-4 ">
+            <h1>Companion Builder</h1>
+
+            <CompanionForm />
+        </article>
+    </main>
   )
 }
 
