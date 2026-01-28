9d9be859da8977102faa01e938fa132a413e6e3f
kinetic4 <pratyushbhargava6@gmail.com>
2026-01-29 04:19:48 +0530

    fixed margin-top

--- app/companion/new/page.tsx
+++ app/companion/new/page.tsx
@@ -10,7 +10,7 @@ const NewCompanion = async () => {
 
   return (
     <main className="lg:w-1/2 md:w-1/3 items-center justify-center">
-        <article className="w-full flex flex-col gap-4 mt-[2rem]">
+        <article className="w-full flex flex-col gap-4">
             <h1>Companion Builder</h1>
 
             <CompanionForm />
