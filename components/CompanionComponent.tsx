a51051ace6d769674060fd29d60a15b7b1f74cb9
kinetic4 <pratyushbhargava6@gmail.com>
2026-01-29 00:57:31 +0530

    Add microphone toggle functionality to CompanionComponent; implement mute state management and update UI with microphone controls.

--- components/CompanionComponent.tsx
+++ components/CompanionComponent.tsx
@@ -21,15 +21,16 @@ const CompanionComponent = ({ companionId, subject, topic, name, userName, userI
 
     const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
     const [isSpeaking, setIsSpeaking] = useState(false);
+    const [isMuted, setIsMuted] = useState(false)
 
     const lottieRef = useRef<LottieRefCurrentProps>(null);
 
     useEffect(() => {
-        if(lottieRef) {
-            if(isSpeaking) {
+        if (lottieRef) {
+            if (isSpeaking) {
                 lottieRef.current?.play()
-            }  else {
-                lottieRef.current?.stop() 
+            } else {
+                lottieRef.current?.stop()
             }
         }
     }, [isSpeaking, lottieRef])
@@ -64,22 +65,58 @@ const CompanionComponent = ({ companionId, subject, topic, name, userName, userI
         }
     }, [])
 
+    const toggleMicrophone = () => {
+        const isMuted = vapi.isMuted();
+        vapi.setMuted(!isMuted)
+        setIsMuted(!isMuted)
+    }
+
+    const handleCall = async () => {
+
+    }
+
+    const handleDisconnect = async () => {
+
+    }
+
     return (
         <section className='flex flex-col h-[70vh]'>
             <section className='flex gap-8 max-sm:flex-col'>
                 <div className='companion-section'>
                     <div className='companion-avatar' style={{ backgroundColor: getSubjectColor(subject) }}>
-                        <div className={cn('absolute transition-opacity duration-1000', callStatus === CallStatus.FINISHED || callStatus === CallStatus.INACTIVE ? 'opacity-1001' : 'opacity-0', callStatus === CallStatus.CONNECTING && 'opacity=100 animate-pulse')}>
-                            <Image src={`/icons/${subject}.svg`} alt={subject} width={150} height={150} className='max-sm:w-fit'/>
+                        <div className={cn('absolute transition-opacity duration-1000', 
+                            callStatus === CallStatus.FINISHED || callStatus === CallStatus.INACTIVE ? 'opacity-1001' : 'opacity-0',
+                            callStatus === CallStatus.CONNECTING && 'opacity=100 animate-pulse')}>
+                            <Image src={`/icons/${subject}.svg`} alt={subject} width={150} height={150} className='max-sm:w-fit' />
                         </div>
-                        <div className={cn('absolute transition-opactiy duration-1000', callStatus === CallStatus.ACTIVE ? "opacity-100": 'opacity-0')}>
-                            <Lottie lottieRef={lottieRef} animationData={soundwaves} autoplay={false} className='companion-lottie'/>
+                        <div className={cn('absolute transition-opactiy duration-1000', callStatus === CallStatus.ACTIVE ? "opacity-100" : 'opacity-0')}>
+                            <Lottie lottieRef={lottieRef} animationData={soundwaves} autoplay={false} className='companion-lottie' />
                         </div>
                     </div>
                     <p className='font-bold text-2xl'>{name}</p>
                 </div>
+                <div className='user-section '>
+                    <div className='user-avatar'>
+                        <Image src={userImage} alt={userName} width={130} height={130} className='rounded-lg' />
+                        <p className='font-bold text-2xl'>
+                            {userName}
+                        </p>
+                    </div>
+                    <button className='btn-mic' onClick={toggleMicrophone}>
+                        <Image src={isMuted ? '/icons/mic-off.svf' : '/icons/mic-on.svg'} alt='mic' width={36} height={36} />
+                        <p className='max-sm:hidden'>
+                            {isMuted ? 'Turn on microphone' : 'Turn off microphone'}
+                        </p>
+                    </button>
+                    <button className={cn('rounded-lg py-2 cursor-pointer transition-colors w-full text-white', 
+                        callStatus === CallStatus.ACTIVE ? 'bg-red-700' : 'bg-primary', callStatus === CallStatus.CONNECTING && 'animate-pulse')} 
+                        onClick={callStatus === CallStatus.ACTIVE ? handleDisconnect : handleCall}
+                        >
+                        {callStatus === CallStatus.ACTIVE ? 'end session' : callStatus === CallStatus.CONNECTING ? "Connecting" : "start Session"}
+                    </button>
+                </div>
                 <div>
-                    
+
                 </div>
             </section>
         </section>
